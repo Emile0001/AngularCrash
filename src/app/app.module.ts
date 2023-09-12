49a5b61829cc2cccf0fce09a3a57659e0c0ef8b6
Emile0001 <boteseh@gmail.com>
2023-09-12 08:37:49 +0200

    property binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StringInterComponent } from './components/string-inter/string-inter.component';
+import { PropBindingComponent } from './components/property-binding/prop-binding.component';
 
 @NgModule({
-  declarations: [AppComponent, StringInterComponent],
+  declarations: [AppComponent, StringInterComponent, PropBindingComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
